c20c2e26d4b437bb48acbdc447d1f2bcf2ad35f7
Siddharth Chandel <siddharthchandel2004@gmail.com>
2023-05-06 22:05:45 +0530

    add compare location

--- CulturTap/routers/locationAPI.py
+++ CulturTap/routers/locationAPI.py
@@ -14,7 +14,8 @@ def get_data(lat: str,long:str):
 @router.get("/compare-location", status_code=status.HTTP_200_OK,)
 def get_data(lat1: float,long1:float,lat2:float,long2:float):
     try:
-        return {"range":lat_long_difference((lat1,long1),(lat2,long2))}
+        result={"value":True} if lat_long_difference((lat1,long1),(lat2,long2)) <=1 else {"value":False}
+        return result
     except Exception as e:
         print(e)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
